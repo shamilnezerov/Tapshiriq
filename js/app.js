9423c3188bcab1810bc49a50b098e84970b5f1e9
shamilnezerov <shamil.nezerov@gmail.com>
2023-07-18 05:24:40 +0400

    18.07.2023-05:24

--- js/app.js
+++ js/app.js
@@ -1,8 +1,31 @@
 function mobilemenu() {
-    var popup = document.getElementById("mobile");
-    if (popup.style.left == "-80%") {
-      popup.style.left = "0%";
-    } else {
-      popup.style.left = "-80%";
-    }
+  var popup = document.getElementById("mobile");
+  if (popup.style.left == "-80%") {
+    popup.style.left = "0%";
+  } else {
+    popup.style.left = "-80%";
   }
+}
+
+window.onscroll = function() {
+  if(window.scrolly > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById('header').style.backgroundColor = 'rgba(184, 228, 218, 1)'
+    document.getElementById('header').style.padding = '3px 0px'
+    document.getElementById('scroll-icon').style.opacity = '1'
+   
+  } else {
+    document.getElementById('header').style.backgroundColor = 'transparent'
+    document.getElementById('header').style.padding = '6px 0px'
+    document.getElementById('scroll-icon').style.opacity = '0'
+  }
+}
+document.getElementById('scroll-icon').addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
+
+
